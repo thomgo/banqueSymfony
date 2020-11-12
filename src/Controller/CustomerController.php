56f1d5f4beb6d672492fd10380fac43a1c951e6b
Thomas <tg.59290@gmail.com>
2020-11-12 11:36:51 +0100

    add login on logout

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -5,7 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+*@IsGranted("IS_AUTHENTICATED_FULLY")
+*/
 class CustomerController extends AbstractController
 {
     /**
